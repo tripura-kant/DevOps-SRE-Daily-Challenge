9716d13d1a73af0f266dfa1657e37821b0d10693
Tripura kant <tripura.kant@zoom.us>
2024-11-27 13:58:03 +0530

    new 2024-11-27

--- Day-1/code.py
+++ Day-1/code.py
@@ -1,7 +1,20 @@
 # Check Disk Usage
 import shutil
-path =
+import os
+import subprocess
+
+print("\n")
+path = "/Users/tripurakant/Documents/"
+stat = shutil.disk_usage(path)
+print("Total disk usage is:", stat.total)
+print("\n")
 # Monitor Running Services
+print("Runnning services are:")
+try:
+    services = subprocess.check_output("ps aux | head -n 6", shell=True).decode("utf-8")
+    print(services)
+except Exception as e:
+    print("Error fetching services:", e)
 # Assess Memory Usage
 # Evaluate CPU Usage
 # Send a Comprehensive Report via Email Every Four Hours
